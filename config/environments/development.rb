2c3ee403c8e2bc5cce0a0be3b7b709591b56d35f
Yoshiyuki Hirano <yhirano@me.com>
2020-03-16 16:03:14 +0900

    Add config.action_mailer.default_url_options to dev config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,3 +60,7 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
+
+Rails.application.configure do
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+end
