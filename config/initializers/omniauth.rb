4581efe3561150fbe745512fff286250c0820988
Yoshiyuki Hirano <yhirano@me.com>
2020-03-16 18:12:24 +0900

    Fix omniauth config

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,11 @@
 require 'omniauth/strategies/doorkeeper'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :doorkeeper, Rails.application.config.x.doorkeeper.client_id, Rails.application.config.x.doorkeeper.client_secret
+  provider(
+    :doorkeeper,
+    Rails.application.config.x.doorkeeper.client_id,
+    Rails.application.config.x.doorkeeper.client_secret,
+    scope: 'email',
+    provider_ignores_state: Rails.env.development?
+  )
 end
