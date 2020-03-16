d27bc0d63c740f269bef8f5737b15045941bb21f
Yoshiyuki Hirano <yhirano@me.com>
2020-03-16 18:11:31 +0900

    Fix callbacks controller

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,4 +1,11 @@
-class CallbacksController < ApplicationController
+class CallbacksController < ActionController::Base
   def handle
+    render json: auth.info
+  end
+
+  private
+
+  def auth
+    request.env['omniauth.auth']
   end
 end
